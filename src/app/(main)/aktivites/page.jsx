7d831fb50573c45343fbf4bf11166198154ac0f7
wu11-mansoureh-safarian-toosi <U2442173@rts-365.dk>
2025-02-10 17:29:34 +0100

    mandagg

--- src/app/(main)/aktivites/page.jsx
+++ src/app/(main)/aktivites/page.jsx
@@ -1,21 +1,22 @@
-import Aktivite from "@/components/aktivite"
-import Footer from "@/components/Footer"
-import { serverFetch } from "@/lib/server-fetch"
-
+import Aktivite from "@/components/aktivite";
+import Footer from "@/components/Footer";
+import { serverFetch } from "@/lib/server-fetch";
 
 export default async function Ejendomme() {
-	const data = await serverFetch("http://localhost:4000/api/v1/activities")
-	console.log(data)
+  const data = await serverFetch("http://localhost:4000/api/v1/activities");
+  console.log(data);
 
-	return (
-		<>
-		<div className="bg-[#5E2E53]">
-			<h1>Alle aAktivites</h1>
-			<ul>
-				{data.map(ejendom => <Aktivite key={ejendom.id} aktivite={ejendom} />)}
-			</ul>
-			</div>
-			<Footer/>
-		</>
-	)
+  return (
+    <>
+      <div className="bg-[#5E2E53]">
+        <h1>Alle aAktivites</h1>
+        <ul>
+          {data.map((ejendom) => (
+            <Aktivite key={ejendom.id} aktivite={ejendom} />
+          ))}
+        </ul>
+      </div>
+      <Footer />
+    </>
+  );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,21 @@
 import Image from "next/image";
-import home from "../assets/home.png"
-import search from "../assets/search.png"
-import folder from "../assets/folder.png"
+import home from "../assets/home.png";
+import search from "../assets/search.png";
+import kalender from "../assets/kalender.png";
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 w-full h-[2em] flex justify-around bg-white p-2">
-        {/* <div className="flex justify-around">  */}
+      {/* <div className="flex justify-around">  */}
       {/* <Link href="/categories"> */}
       <Image src={home} width={22} height={20} alt="home" />
       {/* </Link> */}
       {/* <Link href="/music"> */}
-        <Image src={search} width={22} height={20} alt="search" />
+      <Image src={search} width={22} height={20} alt="search" />
       {/* </Link> */}
       {/* <Image className="absolute bg-orange-500" src={wifi} width={22} height={20} alt="wifi" /> */}
-      <Image src={folder} width={22} height={20} alt="folder" />
+      <Image src={kalender} width={22} height={20} alt="kalender" />
       {/* </div> */}
     </footer>
   );
