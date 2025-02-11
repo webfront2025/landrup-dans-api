d4329956d30cd97b284dc08871c8b799561a6682
wu11-mansoureh-safarian-toosi <U2442173@rts-365.dk>
2025-02-11 15:34:36 +0100

    tiirsdag

--- src/app/(main)/login/page.jsx
+++ src/app/(main)/login/page.jsx
@@ -1,10 +1,17 @@
-import LoginForm from "@/components/login-form"
-
+// import LoginForm from "@/components/login-form"
+import Image from "next/image";
+import logo from "../../../assets/Logo.png"
+import Link from "next/link";
 export default async function Login() {
 	return (
 		<>
+        <div className="bg-[url(/splash-image.jpg)] bg-cover bg-center bg-no-repeat h-screen overflow-hidden">
+        
+        {/* <Image src={logo} width={300} height={300}   alt="logo" className="absolute bottom-[15em]"/> */}
+        <div className="flex justify-center"></div>
 			<h1>Log ind</h1>
-			<LoginForm />
+			{/* <LoginForm /> */}
+            </div>
 		</>
 	)
 }
